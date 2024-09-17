92a5f2fdbb0053691db318eeb7e6b9037f4475c0
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2024-09-17 12:55:46 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import ExpenseTrackerComponent from "@/components/ui/expense-tracker";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ExpenseTrackerComponent/>
+    </div>
+  );
+}
